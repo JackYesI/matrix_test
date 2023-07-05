e943e890b01560315cc410962d4121a010a3bafd
Денис <denus.redchuch@gmail.com>
2023-07-05 21:01:48 +0300

    Update Fourth.py
    Update Fourth.py

--- Fourth.py
+++ Fourth.py
@@ -1,10 +1,33 @@
 import random
 
 matrix = []
+sum_list = []
 
-row = int(input("Enter row: "))
+row = random.randint(1,10)
+col = random.randint(1,10)
 
 for i in range(row):
-    matrix.append(random.randint(1,100))
+    list_row = []
+    for j in range(col):
+        list_row.append(random.randint(1,100))
+    matrix.append(list_row)
 
 print(matrix)
+sum_row = 0
+
+
+for i in range(row):
+    for j in range(col):
+        print(matrix[i][j], end='\t')
+        sum_row += matrix[i][j]
+        if i == 0:
+            sum_list.append(matrix[i][j])
+        else:
+            sum_list[j] += matrix[i][j]
+    print(' | ',sum_row)
+    sum_row = 0
+print('-' * 100)
+
+for item in sum_list:
+    print(item, end='\t')
+
